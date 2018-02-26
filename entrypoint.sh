b2571666e0fd119ac1bdbaa0029bffe17cda203c
whucecil2000 <36835972+whucecil2000@users.noreply.github.com>
2018-02-26 20:22:34 +0800

    us配置
    us配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 cd /v2raybin
 echo -e -n '{"log":{"loglevel":"warning"},"inbound":{"protocol":"shadowsocks","port":' > config.json
 echo -e -n "$PORT" >> config.json
-echo -e -n ',"settings":{"method": "' >> config.json
-echo -e -n 'chacha20' >> config.json
-echo -e -n '","udp": false, "password": "v2ray"}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' >> config.json
+echo -e -n ',"settings":{"clients":[{"id":"' >> config.json
+echo -e -n "$MYUUID" >> config.json
+echo -e -n '","alterId":64,"security":"none"}]},"streamSettings":{"network":"ws"}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' >> config.json
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
 echo -e "$CERT_PEM" > cert.pem
 echo -e "$KEY_PEM"  > key.pem
