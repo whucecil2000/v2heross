81150bfed158d4fb5124efb852876938dd9f5f66
whucecil2000 <36835972+whucecil2000@users.noreply.github.com>
2018-02-26 18:40:51 +0800

    v2ray shadowsocks配置
    v2ray shadowsocks配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 cd /v2raybin
-echo -e -n '{"log":{"loglevel":"warning"},"inbound":{"protocol":"vmess","port":' > config.json
+echo -e -n '{"log":{"loglevel":"warning"},"inbound":{"protocol":"shadowsocks","port":' > config.json
 echo -e -n "$PORT" >> config.json
-echo -e -n ',"settings":{"clients":[{"id":"' >> config.json
-echo -e -n '9e53c877-af25-47ed-9549-118ed80996d1' >> config.json
-echo -e -n '","alterId":64,"security":"none"}]},"streamSettings":{"network":"ws"}},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' >> config.json
+echo -e -n ',"settings":{"method": "' >> config.json
+echo -e -n 'chacha20' >> config.json
+echo -e -n '","ota": true, "password": "Ge-cc123.ms.com"},"inboundDetour":[],"outbound":{"protocol":"freedom","settings":{}}}' >> config.json
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
 echo -e "$CERT_PEM" > cert.pem
 echo -e "$KEY_PEM"  > key.pem
